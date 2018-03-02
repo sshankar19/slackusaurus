e0aa2f6726c5d6d7bb11a080a1d60145bfee3927
Shanks <shanks@havenlife.com>
2018-03-02 13:46:46 -0500

    updating for base restify now

--- src/Slackusaurus.ts
+++ src/Slackusaurus.ts
@@ -1,8 +1,11 @@
-export class Slackusaurus{
+export class Slackusaurus {
 
     private findWord(input: string){
         let pattern = new RegExp("(\[\[.+?\]\])");
         let match = input.match(pattern) as Array<string>;
 
+    }
+    static async makeSmart(blah, blah2) {
+        return {lol: 'sup'};
     }
 }
