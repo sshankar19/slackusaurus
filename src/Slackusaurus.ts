e1d033f89b0612cb564191a19cd5418c289cbc91
Tom D'Alba <tom@havenlife.com>
2018-03-02 14:22:18 -0500

    Added request to package. Added findword and getsyn

--- src/Slackusaurus.ts
+++ src/Slackusaurus.ts
@@ -1,8 +1,28 @@
+
 export class Slackusaurus{
 
-    private findWord(input: string){
+    private findWord(input: string): Array<string>{
         let pattern = new RegExp("(\[\[.+?\]\])");
-        let match = input.match(pattern) as Array<string>;
+        let matches = input.match(pattern) as Array<string>;
+        matches.forEach(word=> {
+            word.replace('[', '');
+            word.replace(']', '');
+        })
 
+        return matches;
+    }
+
+    private getSyn(word: string): string{
+        let request = require('request');
+        let syns = [];
+        request.get('http://words.bighugelabs.com/api/2/cc6b00dacc0971f09759ac9e1acbf530/'+ word + '/')
+        .on('response', function(response) {
+            Object.keys(response).forEach((key,index) => {
+                if(response[key].syn){
+                    syns = syns.concat(response[key].syn);
+                }
+            });
+        });
+        return syns[Math.floor(Math.random()*syns.length)];
     }
 }
