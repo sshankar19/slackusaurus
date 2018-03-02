dfc9d8c9f57b1d0cf90ad8038bc71dfb597ea9ea
Shanks <shanks@havenlife.com>
2018-03-02 15:31:58 -0500

    found errors

--- src/Slackusaurus.ts
+++ src/Slackusaurus.ts
@@ -1,24 +1,29 @@
 import * as util from 'util';
-import * as request from 'request';
+// import * as request from 'request';
+let request = require('request');
 export class Slackusaurus {
-    private static findWords(input: string): Array<string> {
+    private static findWords(input: string): Array<String> {
         let matches = input.match(/\[[^\]]*\]/g);
 
+        let words: Array<String> = [];
+
         matches.forEach(word => {
-            word.replace('[', '');
-            word.replace(']', '');
+            word = word.replace('[', '');
+            word = word.replace(']', '');
+            words.push(word);
         })
 
-        return matches;
+        return words;
     }
 
     private static async getSyn(word: string): Promise<string> {
         let synonyms = [];
         const getAsync = util.promisify(request.get);
         try {
-            const body = 
-                await getAsync({url: 'http://words.bighugelabs.com/api/2/cc6b00dacc0971f09759ac9e1acbf530/' + word + '/json'}, (response) => response.body);
+            const request = 
+                await getAsync({url: 'http://words.bighugelabs.com/api/2/cc6b00dacc0971f09759ac9e1acbf530/' + word + '/json'});
                         
+            let body = JSON.parse(request.body);
             Object.keys(body).forEach((pos, index) => {
                 let posSyns = body[pos].syn;
 
@@ -36,7 +41,7 @@ export class Slackusaurus {
     static async makeSmart(req, res) {
         const text = req.body.text;
         const words = Slackusaurus.findWords(text);
-        const synonyms = words.map(Slackusaurus.getSyn);
+        const synonyms = await Promise.all(words.map(Slackusaurus.getSyn));
         console.log(synonyms);
         return { sup: synonyms };
     }
